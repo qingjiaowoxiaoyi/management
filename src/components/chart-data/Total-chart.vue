244d5d29105a68369cc89b011ba813a7588819aa
qingjiaowoxiaoyi <50947490+qingjiaowoxiaoyi@users.noreply.github.com>
2020-05-01 23:06:19 +0800

    Update Total-chart.vue

--- src/components/chart-data/Total-chart.vue
+++ src/components/chart-data/Total-chart.vue
@@ -7,7 +7,7 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import echarts from "echarts";
-
+import {get,post,put,deletefn} from '@/api/axios';
 @Component({
   name:'TotalChart',
   components: {
@@ -16,12 +16,19 @@ import echarts from "echarts";
 })
 
 export default class TotalChart extends Vue {
+  data:Array<any>=[];
+
+  dateList:Array<any>=[];
+
+  myChart = echarts.init((document as any).getElementById('myEcharts'));
+
   drawLine(){
     // 引入 ECharts 主模块
     var echarts = require('echarts');
 
     // 基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('myEcharts'));
+
     // 绘制图表
     myChart.setOption({
       title: {
@@ -37,24 +44,25 @@ export default class TotalChart extends Vue {
           }
       },
       legend: {
-          data: ['购买总量', '收藏总量']
+          data: ['统计营业额']
       },
       toolbox: {
           feature: {
-              saveAsImage: {}
-          }
+              saveAsImage: {},
+              magicType: {type: ['line', 'bar']},
+          },
       },
       grid: {
           left: '3%',
           right: '4%',
           bottom: '3%',
-          containLabel: true
+          containLabel: true,
       },
       xAxis: [
           {
               type: 'category',
               boundaryGap: false,
-              data: ['2019.12', '2020.01', '2020.02', '2020.03', '2020.04', '2020.05', '2020.06']
+              data: this.dateList
           }
       ],
       yAxis: [
@@ -64,25 +72,48 @@ export default class TotalChart extends Vue {
       ],
       series: [
           {
-              name: '购买总量',
+              name: '统计营业额',
               type: 'line',
-              stack: '总量',
-              areaStyle: {},
-              data: [120, 132, 101, 134, 90, 230, 210]
-          },
-          {
-              name: '收藏总量',
-              type: 'line',
-              stack: '总量',
-              areaStyle: {},
-              data: [220, 182, 191, 234, 290, 330, 310]
+            //   stack: '总量',
+            //   areaStyle: {},
+              data: this.data
           }
       ]
     });
   }
 
+  // 获取新闻api
+  getData(){
+    const loading = this.$Loading;
+    loading.start();
+    get('http://127.0.0.1:3000/admin/revenue')
+    .then(res=>{
+      this.data=(res as any).data;
+      this.drawLine();
+    })
+    .catch(err => {
+       this.$Message.error('加载失败'+err);
+    })
+    .finally(() => {
+      loading.finish();
+    //   this.data = [120, 132, 101, 134, 90, 230, 210];
+    //   this.drawLine();
+    });
+  }
+
   mounted() {
+     for(let i = 0; i<12 ; i++){
+         let mouths = '' as any;
+         if(i+1>=10){
+             mouths = new Date().getFullYear() + '-' + (i + 1);
+         }else{
+             mouths = new Date().getFullYear() + '-0' + (i + 1);
+         }
+         this.dateList.push(mouths)
+     }
+
      this.drawLine(); 
+     this.getData();
   }
 }
 </script>
